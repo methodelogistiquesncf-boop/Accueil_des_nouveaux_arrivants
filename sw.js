ccad79efacca30594c63631728e5409f5c33b68b
methodelogistiquesncf-boop <methodelogistiquesncf@gmail.com>
2026-09-16 14:15:16 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,63 @@
+/* Service worker : coquille de l'app disponible hors-ligne */
+const CACHE = "onboarding-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./css/styles.css",
+  "./js/state.js",
+  "./js/config.js",
+  "./js/utils.js",
+  "./js/auth.js",
+  "./js/renders.js",
+  "./js/planning.js",
+  "./js/modele.js",
+  "./js/users.js",
+  "./js/detail.js",
+  "./js/main.js",
+  "./js/pwa.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  const url = new URL(e.request.url);
+
+  /* Navigations : réseau d'abord (toujours la dernière version), repli cache */
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request)
+        .then(rep => { const copie = rep.clone();
+                       caches.open(CACHE).then(c => c.put("./index.html", copie));
+                       return rep; })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  /* Autres ressources même origine : cache d'abord, puis réseau + mise en cache */
+  if (url.origin === self.location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(hit => hit || fetch(e.request).then(rep => {
+        const copie = rep.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copie));
+        return rep;
+      }))
+    );
+  }
+});
