7d44bbdead27554a5df29596155537ff65e58da3
methodelogistiquesncf-boop <methodelogistiquesncf@gmail.com>
2026-09-16 13:48:42 +0200

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,4 @@
+/* État global partagé entre les modules */
+let idCourant = null, arrivantCourant = null, docsTaches = [], docsPlanning = [], docsUsers = [],
+    editionId = null, unsubsDetail = [], semDebut = null, slotEditId = null, drag = null,
+    monRole = null, unsubProfil = null, msgApresDeconnexion = null, appDemarree = false;
